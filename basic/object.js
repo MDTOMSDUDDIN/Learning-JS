5922521dafe22c50a06bdeeab1d4ab25cb8a8358
MDTOMSDUDDIN <mdtomasuddin1@gmail.com>
2025-01-22 20:15:04 +0600

    js topics

---
+++ basic/object.js
@@ -0,0 +1,4 @@
+const someobject={10:"name",12:"hello"}
+
+// console.log(someobject)
+console.log(someobject[12])
---
+++ basic/object2.js
@@ -0,0 +1,5 @@
+const obj={name:"tomas",addrress:{
+    distract:"sirajgonj",
+    road:"12"
+}}
+console.log(obj);
